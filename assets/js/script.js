660c9b47dfcefe33af891d251e6c2e08e3d596ca
Jayage <javin.li@gmail.com>
2021-05-13 14:01:13 +0000

    jQuery cards slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,23 @@ function initMap() {
     zoom: 8,
   });
 }
+
+
+$(".button-card-1").click(function(){
+   $(".par-1").slideToggle(750); 
+});
+$(".button-card-2").click(function(){
+   $(".par-2").slideToggle(750); 
+});
+$(".button-card-3").click(function(){
+   $(".par-3").slideToggle(750); 
+});
+$(".button-card-4").click(function(){
+   $(".par-4").slideToggle(750); 
+});
+
+
+
+
+
+
