7e6d3c516e16868ab0201eb54f89787f5a1d75fe
Jayage <javin.li@gmail.com>
2021-05-17 21:27:38 +0000

    adding lists

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,20 @@
 function initMap() {
+    
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 9,
     center: { lat: 53.33306 , lng: -6.260 },
     
+
+    
   })};
+
+  function marker(place) {
+      // The marker, positioned at Uluru
+const marker = new google.maps.Marker({
+  position: place,
+  
+  map: map,
+});
+  }
+
+
